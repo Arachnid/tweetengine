0be3aa5df0a76ebc3983a6198b3fb547008487db
Jens W. Klein <jensens@minime.(none)>
2010-01-25 15:03:22 +0100

    simplified buildout

---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="tweetengine",
+    version='1.0',
+    package_dir = {'':'src'},
+    packages=find_packages('src'),
+    zip_safe = True,
+    include_package_data = False,
+    install_requires = ['twitter'],
+    extras_require = dict(test=['zope.testing', 'webtest']),
+)
