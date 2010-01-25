21d08ef0b7cec91edd9d0ef6ecc78241e13b0240
Nick Johnson <arachnid@notdot.net>
2010-01-25 12:41:09 +0100

    Added basic request handler

--- src/tweetengine/__init__.py
+++ src/tweetengine/__init__.py
@@ -1,1 +1,11 @@
-#
+from google.appengine.ext import webapp
+
+
+class MainHandler(webapp.RequestHandler):
+  def get(self):
+    self.response.out.write("Hello, world!")
+
+
+application = webapp.WSGIApplication([
+    ('/', MainHandler),
+], debug=True)
