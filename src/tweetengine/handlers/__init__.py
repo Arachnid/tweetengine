da52455c9181dc494b4cfd5462979c7e99f5aa0b
Jens W. Klein <jensens@minime.(none)>
2010-01-25 18:07:50 +0100

    added settings

--- src/tweetengine/handlers/__init__.py
+++ src/tweetengine/handlers/__init__.py
@@ -1,8 +1,5 @@
 from homepage import HomepageHandler
 from add import AddHandler, CallbackHandler
 from dashboard import DashboardHandler
-<<<<<<< HEAD:src/tweetengine/handlers/__init__.py
 from settings import SettingsHandler
-=======
 from manage import ManageHandler
->>>>>>> edce1ee333632db0fcaad8b10fb58b3f5af70079:src/tweetengine/handlers/__init__.py
