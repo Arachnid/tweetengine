51c3706b8eff15462933028930a1459c342b486f
Jens W. Klein <jensens@minime.(none)>
2010-01-25 15:30:20 +0100

    cfg

--- src/tweetengine/model.py
+++ src/tweetengine/model.py
@@ -33,3 +33,11 @@ class OutgoingTweet(db.Model):
                                      collection_name='approved_tweets')
   message = db.TextProperty(required=True)
   timestamp = db.DateTimeProperty(required=True, auto_now_add=True)
+
+class Configuration(db.Model):
+    secret = db.StringProperty()
+    key = db.StringProperty()
+    
+    @classmethod
+    def instance(cls):
+        return cls.get_or_insert('key')
