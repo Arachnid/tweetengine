cebeef2d1cdac294440c407e5cea34eed5792a1a
Jens W. Klein <jensens@minime.(none)>
2010-01-26 15:18:04 +0100

    remove limit, color number if ngative

--- src/static/tweetengine.js
+++ src/static/tweetengine.js
@@ -1,12 +1,13 @@
-
 jQuery(document).ready(function() {
 	jQuery("#tweetarea").keyup(function(event){
 		var length = 140 - jQuery(this).val().length;
 		if (length < 0) {
-			length = 0;
-			jQuery("#tweetarea").val(
-					jQuery("#tweetarea").val().slice(0, 140)
-			);
+			jQuery("#tweetsubmit").attr('disabled', 'disabled');
+			jQuery("#tweetlabel").attr('class', 'tweet-oversize');
+		};
+		if (length == 0) {
+			jQuery("#tweetsubmit").removeAttr('disabled');
+			jQuery("#tweetlabel").removeAttr('class');
 		};
 		jQuery("#tweetlabel").text(length);
 	});
